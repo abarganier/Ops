73ff228c763910a9ab99343a027e20eff1e3188c
abarganier <abargani@gmail.com>
2017-02-11 15:12:28 -0500

    ASST1 - Finish stoplight solution implementation. Test sp2 now fully passing. Remaining fix was to lock each method to prevent overlapping calls from interfering with each other.

--- kern/synchprobs/stoplight.c
+++ kern/synchprobs/stoplight.c
@@ -69,14 +69,24 @@
 #include <test.h>
 #include <synch.h>
 
+/*
+ * Each quadrant gets its own semaphore
+ */
 struct semaphore* sem0; 
 struct semaphore* sem1; 
 struct semaphore* sem2; 
-struct semaphore* sem3; 
+struct semaphore* sem3;
+
+/*
+ * Each method gets its own lock to prevent overlapping calls from interfering with each other 
+ */
+struct lock* l1;
+struct lock* l2;
+struct lock* l3;
+ 
 /*
  * Called by the driver during initialization.
  */
-
 void
 stoplight_init() {
 	sem0 = sem_create("sem0", 1);
@@ -97,21 +107,37 @@ stoplight_init() {
 	sem3 = sem_create("sem3", 1);
 	if (sem3 == NULL) {
 		panic("Stoplight.c: Failed to create sem3!\n");
-	}			
+	}		
+	l1 = lock_create("lock1");
+	if (l1 == NULL) {
+		panic("Stoplight.c: Failed to create lock l1!\n");
+	}
+	l2 = lock_create("lock2");
+	if (l2 == NULL) { 
+		panic("Stoplight.c: Failed to create lock l2!\n");
+	}	
+	l3 = lock_create("lock3");
+	if (l3 == NULL) {
+		panic("Stoplight.c: Failed to create lock l3!\n");
+	}	
 }
-
 
 /*
  * Called by the driver during teardown.
  */
-
 void stoplight_cleanup() {
 	sem_destroy(sem0);
 	sem_destroy(sem1);
 	sem_destroy(sem2);
 	sem_destroy(sem3);
+	lock_destroy(l1);
+	lock_destroy(l2);
+	lock_destroy(l3);
 }
 
+/*
+ * Used to get a reference to a semaphore given a corresponding quadrant ID
+ */
 struct semaphore* get_sem(uint32_t quadrant) {
 	switch (quadrant) {
 		case 0:
@@ -132,7 +158,6 @@ struct semaphore* get_sem(uint32_t quadrant) {
 	}
 }
 
-
 /*
  * Enters: X
  * Leaves: X
@@ -141,10 +166,12 @@ void
 turnright(uint32_t direction, uint32_t index)
 {
 	struct semaphore* sem = get_sem(direction);
+	lock_acquire(l1);
 	P(sem);
 	inQuadrant(direction, index);
 	leaveIntersection(index);
 	V(sem);
+	lock_release(l1);
 }
 
 /*
@@ -157,6 +184,7 @@ gostraight(uint32_t direction, uint32_t index)
 	struct semaphore* sem_enter = get_sem(direction);
 	uint32_t leave_direction = (direction + 3) % 4;
 	struct semaphore* sem_leave = get_sem(leave_direction);
+	lock_acquire(l2);
 	P(sem_enter);
 	P(sem_leave);
 	inQuadrant(direction, index);
@@ -164,7 +192,9 @@ gostraight(uint32_t direction, uint32_t index)
 	leaveIntersection(index);
 	V(sem_enter);
 	V(sem_leave);
+	lock_release(l2);
 }
+
 /*
  * Enters: X
  * Passes through: (X+3)%4
@@ -178,14 +208,16 @@ turnleft(uint32_t direction, uint32_t index)
 	uint32_t direction_leave = (direction + 2) % 4;
 	struct semaphore* sem_passthrough = get_sem(direction_passthrough);
 	struct semaphore* sem_leave = get_sem(direction_leave);
+	lock_acquire(l3);
 	P(sem_enter);
 	P(sem_passthrough);
 	P(sem_leave);
 	inQuadrant(direction, index);
-	inQuadrant(direction_passthrough, index);
-	inQuadrant(direction_leave, index);
+	inQuadrant(direction_passthrough, index);	
+	inQuadrant(direction_leave, index);	
 	leaveIntersection(index);
 	V(sem_enter);
 	V(sem_passthrough);
 	V(sem_leave);
+	lock_release(l3);
 }
