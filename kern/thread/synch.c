6730947ccb32b4d576da13c99c94e86a64163be5
abarganier <abargani@gmail.com>
2017-02-15 20:35:14 +0000

    ASST1: move wchan_wakeall calls into the spinlock critical section in rwlock_release_write

--- kern/thread/synch.c
+++ kern/thread/synch.c
@@ -408,7 +408,7 @@ rwlock_release_write(struct rwlock *rw) {
 	spinlock_acquire(&rw->rw_spinlock);
 	KASSERT(rw->w_exec);
 	rw->w_exec = false;
-	spinlock_release(&rw->rw_spinlock);
 	wchan_wakeall(rw->w_wchan, &rw->rw_spinlock);
 	wchan_wakeall(rw->r_wchan, &rw->rw_spinlock);
+	spinlock_release(&rw->rw_spinlock);
 }
